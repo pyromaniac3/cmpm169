faab3905ca4b0fab937b28d89dc77c863089dd64
Athena Patronas <97550706+pyromaniac3@users.noreply.github.com>
2024-01-20 11:50:35 -0800

    example

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -2,66 +2,33 @@
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let xoff = 0; // both controls randomness of the noise
+let yoff = 0;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(800, 500);
+  background(0);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  building();
+  /*for(let i = 0; i < 10; i++){
+    building();
+  }*/
+  // click on the screen to make a building there 
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function building(){
+  stroke(255); // setting stroke color to white
+  noFill(); // won't fill between the strokes
+  beginShape(); 
+  let xoff = 0; 
+  for (let x = 0; x <= width; x += 10) { 
+    let xNoise = map(noise(xoff), 0, 1, 0, width); // this is whats making it not be the whole width
+    let y = map(noise(yoff), 0, 1, height, 0); // Generate a y-coordinate using noise
+    vertex(xNoise, y); // create a vertex from the x and y-coordinate
+    xoff += 0.01; // change the number to change the pattern
+  }
+  yoff += 0.01;
+  endShape();
 }
