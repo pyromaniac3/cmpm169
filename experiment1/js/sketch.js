6b2d92325f95d60dca92934b353f65c692e9ac36
Athena Patronas <97550706+pyromaniac3@users.noreply.github.com>
2024-01-22 20:05:06 -0800

    updates experiment 1 and some other minor touches

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -1,10 +1,6 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
-
-let xoff = 0; // both controls randomness of the noise
-let yoff = 0;
-let canvasContainer;
+// sketch.js - just to be a pretty picture underneath the getting started page
+// Author: Athena Patronas
+// Date: 1/15/24
 
 function setup() {
   // place our canvas, making it fit our container
@@ -16,28 +12,8 @@ function setup() {
       console.log("Resizing...");
       resizeCanvas(canvasContainer.width(), canvasContainer.height());
   });
-  background(0);
+  background(46,139,87);
 }
 
 function draw() {
-  building();
-  /*for(let i = 0; i < 10; i++){
-    building();
-  }*/
-  // click on the screen to make a building there 
-}
-
-function building(){
-  stroke(255); // setting stroke color to white
-  noFill(); // won't fill between the strokes
-  beginShape(); 
-  let xoff = 0; 
-  for (let x = 0; x <= width; x += 10) { 
-    let xNoise = map(noise(xoff), 0, 1, 0, width); // this is whats making it not be the whole width
-    let y = map(noise(yoff), 0, 1, height, 0); // Generate a y-coordinate using noise
-    vertex(xNoise, y); // create a vertex from the x and y-coordinate
-    xoff += 0.01; // change the number to change the pattern
-  }
-  yoff += 0.01;
-  endShape();
 }
