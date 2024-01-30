c917cf5b68b7c26d1cd01d04dd35982aa67aa3ed
pyromaniac3 <97550706+pyromaniac3@users.noreply.github.com>
2024-01-29 16:28:13 -0800

    adjusted so it would look better on the website

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -45,15 +45,7 @@ function preload() {
 }
 
 function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-  $(window).resize(function() {
-      console.log("Resizing...");
-      resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  });
+  createCanvas(1200, 600);
   imageMode(CENTER);
   // set the current shape to the first in the array
   currentShape = shapes[0];
